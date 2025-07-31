aeaf805fa50146a53d24a03635bf5916a4fb4b2b
Ahmed <ahmed.mabohamer200@gmail.com>
2025-07-31 16:07:37 +0300

    handle pay mob in campigns

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,10 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import organizationReducer from "./Slices/organizationsSlice";
+import campaignsReducer from "./Slices/campaignsSlice";
+import paymentReducer from "./Slices/paymentSlice";
 
-import { configureStore } from '@reduxjs/toolkit';
-import organizationReducer from './Slices/organizationsSlice'
-import campaignsReducer from'./Slices/campaignsSlice'
 export const store = configureStore({
-    reducer: {
-        organizations: organizationReducer,
-        campaigns: campaignsReducer,
-    },
+  reducer: {
+    organizations: organizationReducer,
+    campaigns: campaignsReducer,
+    payment: paymentReducer,
+  },
 });
