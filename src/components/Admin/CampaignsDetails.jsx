726bf001c06d01580e6cbed8d3d6b41b3c32aec3
Dell <amerakhames42@gmail.com>
2025-08-01 03:15:16 +0300

    edit pages

--- src/components/Admin/CampaignsDetails.jsx
+++ src/components/Admin/CampaignsDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router';
 import { getCampaignById } from '../../redux/Slices/campaignsSlice';
@@ -9,53 +9,94 @@ import {
   CardContent,
   CardMedia,
   Chip,
+  Container,
   Divider,
   Grid,
   Stack,
   Typography
 } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const CampaignsDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { selectedCampaign: campaign, loading, error } = useSelector((state) => state.campaigns);
+  const [mainImage, setMainImage] = useState(campaign?.cover_image)
 
   useEffect(() => {
     dispatch(getCampaignById(id));
-  }, [id, dispatch]);
-
+  }, [id, dispatch ]);
+  useEffect(() => {
+    if (campaign?.cover_image) {
+      setMainImage(campaign.cover_image);
+    }
+  }, [campaign]);
   if (loading) return <Typography>جارٍ التحميل...</Typography>;
   if (error) return <Typography color="error">حدث خطأ: {error}</Typography>;
   if (!campaign) return <Typography>لم يتم العثور على المشروع.</Typography>;
 
   return (
-    <Box sx={{ px: { xs: 2, sm: 5 }, py: 3 }}>
-      <Button onClick={() => navigate('/admin/main/campaigns')} sx={{ mb: 2 }}>
-        العودة إلى جميع المشاريع
-      </Button>
+    <>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Grid container spacing={3} alignItems="start" sx={{ position: 'relative', display: "flex", flexDirection: "column" }} >
 
-      <Card sx={{ boxShadow: 3, borderRadius: 3, overflow: 'hidden' }}>
-        {campaign.cover_image && (
-          <CardMedia
-            component="img"
-            image={campaign.cover_image}
-            alt={campaign.name}
-            sx={{ height: 300, objectFit: 'cover' }}
-          />
-        )}
+          {/* Campaign images */}
+          <Grid item size={{ xs: 12, sm: 8 }} >
+            <Box component={"img"} src={mainImage} sx={{
+              width:"100%",
+              height:400,
+              objectFit:'cover'
+            }} />
+            <Button onClick={() => navigate('/admin/main/campaigns')} sx={{ mb: 3, position: "absolute", top: 0, left: 0 }}>
+              <Typography>العوده الي جميع المشاريع</Typography>  <ArrowBackIosIcon sx={{ color: 'primary.main' }} />
+            </Button>
+            <Box  size={12}>
 
-        <CardContent>
-          <Typography variant="h5" fontWeight="bold" gutterBottom>
-            {campaign.name}
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary" mb={2}>
-            {campaign.description || 'لا يوجد وصف'}
-          </Typography>
-
+            {campaign.campaign_images.map((img, idx) => (
+              <Box
+              key={idx}
+              component="img"
+              src={img}
+              alt={`campaign-img-${idx}`}
+              sx={{
+                width: 100,
+                height: 100,
+                objectFit: 'cover',
+                borderRadius: 2,
+                boxShadow: 1,
+                ml:1
+              }}
+              onClick={()=>{setMainImage(img)}}
+              />
+            ))}
+            <Box
+              component="img"
+              src={campaign.cover_image}
+              alt={``}
+              sx={{
+                width: 100,
+                height: 100,
+                objectFit: 'cover',
+                borderRadius: 2,
+                boxShadow: 1,
+              }}
+                onClick={() => { setMainImage(campaign.cover_image) }}
+              />
+              </Box>
+          </Grid>
+          {/* campaign details */}
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item sm={6}>
+              <Typography variant="h5" fontWeight="bold" >
+                {campaign.name}
+              </Typography>
+
+              <Typography variant="body1" color="text.secondary" mb={2}>
+                {campaign.description || 'لا يوجد وصف'}
+              </Typography>
+            </Grid>
+            <Grid item size={{xs:12,sm:6}}>
               <Stack spacing={1}>
                 <Typography variant="subtitle2">نوع المشروع: {campaign.type}</Typography>
                 <Typography variant="subtitle2">تاريخ البداية: {campaign.start_date}</Typography>
@@ -64,50 +105,27 @@ const CampaignsDetails = () => {
                 <Typography variant="subtitle2">تم الإنشاء: {new Date(campaign.created_at).toLocaleString()}</Typography>
               </Stack>
             </Grid>
-            <Divider sx={{ mx: 4, border: 1 }} />
+            <Divider sx={{ mx: 4, border: 1 }}  />
             <Grid item xs={12} sm={6}>
               <Stack spacing={1}>
-                <Typography variant="subtitle2">عدد المتطوعين الحالي: {campaign.current_volunteers}</Typography>
+                {campaign.current_volunteers && (<Typography variant="subtitle2">عدد المتطوعين الحالي: {campaign.current_volunteers}</Typography>)}
                 <Typography variant="subtitle2">العدد المستهدف للمتطوعين: {campaign.goal_volunteers}</Typography>
-                <Typography variant="subtitle2">المبلغ الحالي: {campaign.current_amount} ج.م</Typography>
+                {campaign.current_amount && (<Typography variant="subtitle2">المبلغ الحالي: {campaign.current_amount} ج.م</Typography>)}
                 <Typography variant="subtitle2">المبلغ المستهدف: {campaign.goal_amount} ج.م</Typography>
                 <Chip
                   label={campaign.is_approved ? 'معتمد' : 'غير معتمد'}
-                  color={campaign.is_approved ? 'success' : 'warning'}
+                  color={campaign.is_approved ? 'primary' : 'warning'}
                   size="small"
                 />
               </Stack>
             </Grid>
           </Grid>
+        </Grid>
 
-          {campaign.campaign_images?.length > 0 && (
-            <>
-              <Divider sx={{ my: 3 }} />
-              <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
-                صور المشروع
-              </Typography>
-              <Stack direction="row" spacing={2} sx={{ flexWrap: 'wrap' }}>
-                {campaign.campaign_images.map((img, idx) => (
-                  <Box
-                    key={idx}
-                    component="img"
-                    src={img}
-                    alt={`campaign-img-${idx}`}
-                    sx={{
-                      width: 100,
-                      height: 100,
-                      objectFit: 'cover',
-                      borderRadius: 2,
-                      boxShadow: 1,
-                    }}
-                  />
-                ))}
-              </Stack>
-            </>
-          )}
-        </CardContent>
-      </Card>
-    </Box>
+      </Container>
+
+    </>
+
   );
 };
 
