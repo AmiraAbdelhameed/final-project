5708028bbfdada4dc9f44ac1006ffdd7d40b9533
Dell <amerakhames42@gmail.com>
2025-08-07 15:58:31 +0300

    edit see more projects

---
+++ src/components/Admin/index.js
@@ -0,0 +1,24 @@
+import Search from './Search';
+import AdminCampaigns from './AdminCampaigns';
+import AdminCard from './AdminCard';
+import AdminOrganizationDetails from './AdminOrganizationDetails';
+import CampaignsDetails from './CampaignsDetails';
+import Organization from './Organization';
+import PaymentReq from './PaymentReq';
+import ProtectedAdminRoute from './ProtectedAdminRoute';
+import Sidebar from './Sidebar';
+
+
+export{
+    Search,
+    AdminCampaigns,
+    AdminCard,
+    AdminOrganizationDetails,
+    CampaignsDetails,
+    Organization,
+    PaymentReq,
+    ProtectedAdminRoute,
+    Sidebar
+}
+
+
